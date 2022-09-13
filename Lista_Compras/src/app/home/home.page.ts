4c800820df4c5fda83b99fdcb6bdb980ec6f6ae8
Luis <luis.aquino@edu.unifil.br>
2022-09-13 20:31:39 -0300

    soma valores

--- Lista_Compras/src/app/home/home.page.ts
+++ Lista_Compras/src/app/home/home.page.ts
@@ -11,29 +11,40 @@ export class HomePage {
 
   constructor(private storage:Storage) {
     this.storage.create();
+
   }
 
   ngOnInit(){
     this.atualizaLista();
+    this.somarPrecos()
   }
 
   variavel_lista = [];
   texto: string = "";
   preco: number = 0;
   aux = 0;
+  precoTotal: number = 0
 
   async adiciona() {
     if (!(this.texto == "" || this.preco == 0)) {
-      //this.variavel_lista.push("0", this.texto);
+
+        //this.variavel_lista.push("0", this.texto);
+     
+      
 
       this.variavel_lista.forEach(item => {
         if(parseInt(item[0]) > this.aux) {
-          this.aux = parseInt(item[0]);
+          this.aux = parseInt((item[0]));
         }
       })
+      
       this.aux = this.aux + 1;
       await this.storage.set(this.aux.toString(), [this.texto, this.preco]);
+      
       this.atualizaLista();
+
+
+      
       this.texto = "";
       this.preco = 0;
     }
@@ -48,10 +59,21 @@ export class HomePage {
 
   }
 
-  atualizaLista() {
+   atualizaLista() {
+    
     this.variavel_lista = [];
     this.storage.forEach((value, key, index) => {
       this.variavel_lista.push([key, value]);
+      
+    })
+    this.somarPrecos()
+  }
+
+  somarPrecos(){
+    this.precoTotal = 0
+    this.storage.forEach((value, key, index) => {
+      this.precoTotal += parseFloat(value[1])
+      console.log("preco:", this.precoTotal)
     })
   }
 
